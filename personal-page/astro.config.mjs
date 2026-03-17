b9b597809c5c9b91d94a335206a25911840898b2
Hajdu Csaba <kyberszittya@protonmail.ch>
2026-03-17 11:36:33 +0100

    Config extended

--- personal-page/astro.config.mjs
+++ personal-page/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kyberszittya.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
